de16b53ff9f17fe2d8b39150ad5b31244ede810f
Jitprakash <jitprakash2020@gmail.com>
2025-08-24 15:23:09 +0530

    Resume feedback page done

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -63,8 +63,7 @@ const Upload = () => {
         data.feedback = JSON.parse(feedbackText);
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, redirecting...');
-        console.log(data);
-
+        navigate(`/resume/${uuid}`);
     }
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
